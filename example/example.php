c608b275a923557372fda530efa852fca41c94fc
Manuel Reinhard <manu@sprain.ch>
2019-01-25 09:13:48 +0100

    Make path in example absolute

--- example/example.php
+++ example/example.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once('../vendor/autoload.php');
+require_once(__DIR__ . '/../vendor/autoload.php');
 
 $htmlPhpExcel = new \Ticketpark\HtmlPhpExcel\HtmlPhpExcel('example.html');
 $htmlPhpExcel->process()->output();
