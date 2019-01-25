360e3ea6713e43a2626233dd95f8f2d2f09a6ab9
Manuel Reinhard <manu@sprain.ch>
2019-01-25 12:46:39 +0100

    Change example to save a local test file

--- example/example.php
+++ example/example.php
@@ -2,5 +2,5 @@
 
 require_once(__DIR__ . '/../vendor/autoload.php');
 
-$htmlPhpExcel = new \Ticketpark\HtmlPhpExcel\HtmlPhpExcel('example.html');
-$htmlPhpExcel->process()->output();
+$htmlPhpExcel = new \Ticketpark\HtmlPhpExcel\HtmlPhpExcel(__DIR__  . '/example.html');
+$htmlPhpExcel->process()->save(__DIR__ . '/test.xlsx');
